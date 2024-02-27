c937fba0abd800cd9a450009c84f3b907e4b44d8
peagace <ph_alencar1@hotmail.com>
2024-02-26 23:10:14 -0300

    nftoyer project

--- app/coin/page.js
+++ app/coin/page.js
@@ -10,28 +10,28 @@ const coin = () => {
       <ProjectHeader title={'FMP x TELLA'} category={'CONCEPT DESIGN / 3D / ANIMATION'} year={'2023'}/>
 
       <div className='w-full h-full bg-lightgray pt-4 px-4'>
-        <Image src="/assets/projects/coin/coin-01.png" height={720} width={1880} className='self-center' />   
+        <Image src="/assets/projects/coin/coin-01.png" height={720} width={18800} className='self-center' quality={100} unoptimized='true'/>   
 
-        <video width="1920" height="1080" loop autoPlay muted className='pt-4'>
+        <video width="19200" height="1080" loop autoPlay muted className='pt-4'>
               <source src="/assets/projects/coin/coin-02.mp4" type="video/mp4" />
         </video>
 
-        <Image src="/assets/projects/coin/coin-03.png" height={720} width={1880} className='self-center pt-4'/>   
+        <Image src="/assets/projects/coin/coin-03.png" height={720} width={18800} className='self-center pt-4' quality={100} unoptimized='true'/>   
 
-        <Image src="/assets/projects/coin/coin-04.png" height={1800} width={1880} className='self-center pt-4'/>   
+        <Image src="/assets/projects/coin/coin-04.png" height={1800} width={18800} className='self-center pt-4' quality={100} unoptimized='true'/>   
 
         <div className='grid grid-cols-12'>
           <div className='lg:col-span-6 col-span-12'>
-            <Image src="/assets/projects/coin/coin-05.png" height={930} width={1468} className='self-center pt-4 lg:pr-2 pr-0'/> 
+            <Image src="/assets/projects/coin/coin-05.png" height={930} width={14680} className='self-center pt-4 lg:pr-2 pr-0' quality={100} unoptimized='true'/> 
           </div>
 
           <div className='lg:col-span-6 col-span-12'>
-          <Image src="/assets/projects/coin/coin-06.png" height={930} width={1468} className='self-center pt-4 lg:pl-2 pl-0'/> 
+          <Image src="/assets/projects/coin/coin-06.png" height={930} width={14680} className='self-center pt-4 lg:pl-2 pl-0' quality={100} unoptimized='true'/> 
           </div>
         </div>
 
-        <video width="1880" height="1800" loop autoPlay muted className='pt-4'>
-              <source src="/assets/projects/coin/coin-07.mp4" type="video/mp4" />
+        <video width="18800" height="1800" loop autoPlay muted className='pt-4'>
+              <source src="/assets/projects/coin/coin-07.mp4" type="video/mp4"/>
         </video>
       </div>
       
--- app/nftoyer/page.js
+++ app/nftoyer/page.js
@@ -1,13 +1,65 @@
 'use client'
 import * as React from 'react';
 import ProjectHeader from '@/components/project-header/ProjectHeader';
+import Image from 'next/image';
 
 
 const nftoyer = () => {
 
   return (
     <>
-      <ProjectHeader title={'NFTOYER'} category={'PRODUCT DESIGN / 3D'} year={'2022'}/>
+      <ProjectHeader title={'NFTOYER'} category={'PRODUCT DESIGN / 3D'} year={'2022'} />
+
+      <div className='w-full h-full bg-lightgray pt-4 px-4'>
+        <Image src="/assets/projects/nftoyer/nftoyer-01.png" height={3018} width={18800} className='self-center' quality={100} unoptimized='true'/>
+
+        <Image src="/assets/projects/nftoyer/nftoyer-02.png" height={1464} width={18800} className='self-center pt-4' quality={100} unoptimized='true'/>
+
+        <div className='grid grid-cols-12 gap-4'>
+          <div className='lg:col-span-4 col-span-12'>
+            <Image src="/assets/projects/nftoyer/nftoyer-03.png" height={998} width={14680} className='self-center lg:pt-4 pt-4' quality={100} unoptimized='true'/>
+          </div>
+
+          <div className='lg:col-span-4 col-span-12'>
+            <Image src="/assets/projects/nftoyer/nftoyer-04.png" height={998} width={14680} className='self-center lg:pt-4 pt-0' quality={100} unoptimized='true'/>
+          </div>
+
+          <div className='lg:col-span-4 col-span-12'>
+            <Image src="/assets/projects/nftoyer/nftoyer-05.png" height={998} width={14680} className='self-center lg:pt-4 pt-0' quality={100} unoptimized='true'/>
+          </div>
+        </div>
+
+        <Image src="/assets/projects/nftoyer/nftoyer-06.png" height={884} width={18800} className='self-center pt-4' quality={100} unoptimized='true'/>
+
+        <div className='grid grid-cols-12'>          
+            <div className='lg:col-span-4 col-span-12 flex flex-col'>
+              <Image src="/assets/projects/nftoyer/nftoyer-07.png" height={1185} width={6240} className='self-center pt-4 lg:pr-4 pr-0' quality={100} unoptimized='true'/>
+
+              <Image src="/assets/projects/nftoyer/nftoyer-08.png" height={932} width={6240} className='self-end pt-4 lg:pr-4 pr-0' quality={100} unoptimized='true'/>
+            </div>
+
+            <div className='lg:col-span-8 col-span-12'>
+              <Image src="/assets/projects/nftoyer/nftoyer-09.png" height={2055} width={6240} className='self-center pt-4 h-full' quality={100} unoptimized='true'/>
+            </div>         
+        </div>
+
+        <Image src="/assets/projects/nftoyer/nftoyer-10.png" height={672} width={18800} className='self-center pt-4' quality={100} unoptimized='true'/>
+
+        <div className='grid grid-cols-12'>
+          <div className='lg:col-span-6 col-span-12'>
+            <Image src="/assets/projects/nftoyer/nftoyer-11.png" height={930} width={14680} className='self-center pt-4 lg:pr-2 pr-0' quality={100} unoptimized='true'/>
+          </div>
+
+          <div className='lg:col-span-6 col-span-12'>
+            <video width="19200" height="930" loop autoPlay muted className='pt-4 lg:pl-2 pl-0'>
+              <source src="/assets/projects/nftoyer/nftoyer-12-video.mp4" type="video/mp4" />
+            </video>
+          </div>          
+        </div>
+
+        <Image src="/assets/projects/nftoyer/nftoyer-13.png" height={1135} width={18800} className='self-center pt-4' quality={100} unoptimized='true'/>
+
+      </div>
     </>
   )
 }
