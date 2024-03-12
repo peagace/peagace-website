0b602ab3b3dca918f4d11d04f7d3c5e1a453fbc8
peagace <ph_alencar1@hotmail.com>
2024-03-12 11:08:21 -0300

    meta fix

--- data/meta-data.js
+++ data/meta-data.js
@@ -1,7 +1,7 @@
 const metaData = {
     title: "PEAGACE ©",
-    description: "Helping worlwide brands to create unique digital projects from scratch blending art and design seamlessly.",
-    subtitle: "WORLWIDE DESIGNER.",
+    description: "Helping worldwide brands to create unique digital projects from scratch blending art and design seamlessly.",
+    subtitle: "WORLDWIDE DESIGNER.",
     keywords: ["PEAGACE", "PHC", "DESIGN", "DESIGNER", "PEDRO HENRIQUE ALENCAR BATISTA", "3D DESIGN", "3D MOTION", "PRODUCT DESIGN", "ANIMATION"],
     themeColor: "#FF0F00",
     url: "https://peagace.com",
